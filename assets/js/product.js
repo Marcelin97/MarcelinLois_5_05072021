95a3e28464f51ed03b6376cc8d5a06de69cbf912
Loïs <lois_m@outlook.com>
2021-08-25 23:56:16 +0200

    How to update the quantity with the same article on the localStorage

--- assets/js/product.js
+++ assets/js/product.js
@@ -53,7 +53,7 @@ function renderProduct(product) {
   document.getElementsByTagName("img")[0].src = product.imageUrl;
   document.getElementsByTagName("h1")[0].innerHTML = product.name;
   document.getElementsByTagName("select")[0].innerHTML =
-    '<option value="hidden">Veuillez choisir une option</option>' +
+    '<option value="">Veuillez choisir une option</option>' +
     getOptions(product[getCustomisation(product)]);
   document.getElementsByName("price")[0].innerHTML = index.priceToEuros(
     product.price
@@ -65,7 +65,49 @@ getProduct().then((result) => {
         throw new Error("Product not found");
       }
   renderProduct(result);
+  document.getElementsByTagName('form')[0].addEventListener('submit', function (event) {
+    // Envoie valeur à localStorage après soumission du formulaire
+
+    event.preventDefault(); //on stop la propagation
+    let cart = {};
+    if (localStorage.getItem("cart") != null) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+
+    let key = result.name + "-" + document.getElementById("options").value;
+    key = key.replace(" ", "_");
+
+    let product = {
+      // Cette fonction assigne les valeurs à envoyer à localStorage
+      _id: result._id,
+      name: result.name,
+      description: result.description,
+      imageUrl: result.imageUrl,
+      price: result.price,
+      optionValue: document.getElementById("options").value,
+      qty: Number(document.getElementById("quantity").value),
+    };
+    cart[key] = product;
+    localStorage.setItem("cart", JSON.stringify(cart));
+  });
 });
+
+
+//fonction pour ajouter un article déjà existante dans mon panier en ajoutant uniquement la quantité à mon localStorage et pas l'article complet
+function ModifyItem () {
+        let key = result.name + "-" + document.getElementById("options").value;
+    // vérifie si le produit existe déjà dans le panier
+  
+// Vérifie si la clé existe.
+            if (localStorage.getItem ("key")!= null)
+            {
+              //mettre à jour
+              localStorage.setItem (qty);
+              document.getElementById("quantity").value = localStorage.getItem (qty);
+            }
+        
+    doShowAll ();
+}
 
 function getCustomisation(product) {
   if ("colors" in product) return "colors";
@@ -105,3 +147,4 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //Button drecrement and increment with my input
+
