ce10d1146d6a6e1319b0192e620cb87dd3f27ec5
Loïs <lois_m@outlook.com>
2021-09-08 16:07:12 +0200

    btnRemove on items on the cart and update cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -8,45 +8,6 @@ index.incrementDecrement();
 //Button decrement and increment with my input//
 ////////////////////////////////////////////////
 
-// ////////////////////////////////////////////////////////////////////////////////////////
-// ///////////////////////////////////////////////
-// ////dynamic display of products in the cart/////
-// ///////////////////////////////////////////////
-
-// //sélection de la classe ou je vais injecter le code HTML
-// let containerCart = document.getElementsByClassName("container-cart");
-// console.log(containerCart);
-// //sélection de la classe ou je vais injecter le code HTML
-
-// //je déclare la structure de mon nouveau panier
-// let structureProduitPanier = [];
-// //je déclare la structure de mon nouveau panier
-
-// //si le panier est vide : afficher le panier est vide
-// if (cart === null) {
-
-// displayCart();
-
-// // function displayCart() {
-// //   let items = document.getElementById("items");
-// //   let listOfProductsTitle = document.createElement("h2");
-// //   listOfProductsTitle.textContent = "Voici la liste de vos produits";
-// //   items.appendChild(listOfProductsTitle);
-// //   for (let i = 0; i < containerCart.length; i++) {
-// //     const product = containerCart[i];
-// //     items.innerHTML +=
-// //       `
-
-// //     `;
-// //   }
-// // }
-
-// //   //injection du html dans la page panier
-// //   if (k == containerCart.length) {
-// //       containerCart.innerHTML = structureProduitPanier;
-// //     }
-// // }
-
 // ///////////////////////////////////////////////
 // ////////I get my cart from LocalStorage////////
 // ///////////////////////////////////////////////
@@ -124,7 +85,7 @@ function renderCartProduct(product) {
                               </div>
                           </div>
                           <div class="info-items-remove">
-                            <a href="#">Supprimer</a>
+                            <a href="#">Supprimer l'article</a>
                           </div>
                         </div>
                     </div>
@@ -132,75 +93,46 @@ function renderCartProduct(product) {
                 </li>
               </ul>`;
     document.getElementById("items").innerHTML = fullPanier;
-    // const product = cart[i];
-    //   document.getElementsByTagName("img")[i].src = product.imageUrl;
-    //   document.getElementsByClassName("titreProduct")[i].innerHTML = product.name;
-    // document.getElementsByClassName("description")[i].innerHTML = product.description;
-    // document.getElementsByName("qty")[i].innerHTML = product.qty + " article(s) dans le panier";
-    // document.getElementsByName("option")[i].innerHTML =
-    //   "Voici l'option que vous avez choisi: " + product.optionValue;
-    //   document.getElementsByName("price")[0].innerHTML = index.priceToEuros(
-    //     product.price
-    //   );
-
-    //Add the product in the first section
-    // containerCart.innerHTML +=
-    //   `;
-    //               <ul>
-    //           <li>
-    //             <div class="details-items">
-    //               <div class="details-items-img">
-    //                 <div>
-    //                   <a href="./product.html?id= ${product.name}">
-    //                     <img
-    //                     src="` +
-    //   product.imageUrl +
-    //   `"
-    //                     alt="Appareil photo vintage sur un gard-corp bois en extérieur"
-    //                     >
-    //                   </a>
-    //                 </div>
-    //               </div>
-    //               <div class="info-items">
-    //                 <div>
-    //                   <div class="info-items-desc">
-    //                     <p id="titre-product">` +
-    //   product.name +
-    //   `</p>
-    //                     <p id="description">` +
-    //   product.description +
-    //   `
-    //                       </p>
-    //                     <div>
-    //                       <span>` +
-    //   index.priceToEuros(product.price) +
-    //   `</span>
-    //                     </div>
-    //                   </div>
-    //                   <div class="info-items-quantity">
-    //                     <div>
-    //                         <div class="cart-update">
-    //                           <button type="button" class="btn-update" id="decrement" value="-1"><i class="fas fa-minus"></i></button>
-    //                           <input type="number" id="quantity" value="1">
-    //                           <button type="button" class="btn-update" id="increment" value="+1"><i class="fas fa-plus"></i></button>
-    //                         </div>
-    //                         <div class="info-items-total">
-    //                           <span>` +
-    //   index.priceToEuros(product.price) +
-    //   `</span>
-    //                         </div>
-    //                     </div>
-    //                     <div class="info-items-remove">
-    //                       <a href="#">Supprimer</a>
-    //                     </div>
-    //                   </div>
-    //               </div>
-    //             </div>
-    //           </li>
-    //         </ul>
-    // `;
   }
 }
 // ///////////////////////////////////////////////
 // ////dynamic display of products in the cart/////
 // ///////////////////////////////////////////////
+
+
+// function removeItemFromCart(name) {
+
+// ///////////////////////////////////////////////
+// ////////////removeOneItemsOnTheCart/////////////
+// ///////////////////////////////////////////////
+let btnRemove = document.querySelectorAll(".info-items-remove");
+btnRemove.forEach(function (element, index, array) {
+  btnRemove[index].addEventListener("click", function () {
+    if (cart.length > 1) {
+      cart.splice(index, 1);
+      //on envoie la variable dans le local Storage
+      //la transformation en format JSON
+      localStorage.setItem("cart", JSON.stringify(cart));
+      location.reload();
+    } else {
+      localStorage.removeItem("cart");
+      location.reload();
+    }
+  })
+})
+// ///////////////////////////////////////////////
+// ////////////removeOneItemsOnTheCart/////////////
+// ///////////////////////////////////////////////
+
+// function clearCarts() {
+//   // Remove all datas in localStorage
+//   localStorage.clear();
+
+//   // Remove each element in the cart view
+//   document.querySelectorAll("artilcle").forEach((element) => {
+//     element.remove();
+//   });
+
+//   document.getElementById("cart-container").innerHTML =
+//     "<p>Votre panier est vide.</p>";
+// }
