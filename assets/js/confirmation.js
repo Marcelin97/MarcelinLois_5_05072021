98679da4b887d663e5718f6b9bcfc5e52960a18c
Loïs <lois_m@outlook.com>
2021-09-14 01:33:44 +0200

    start confirmation

---
+++ assets/js/confirmation.js
@@ -0,0 +1,22 @@
+let Id = localStorage.getItem("idresponse");
+// let ordersInformation = localStorage.getItem("formValues");
+// console.log("ordersInformation");
+
+document.querySelector(
+  "#orderCommande"
+).textContent = `Voici ton numéro de commande : ${Id} `;
+document.querySelector(
+  "#orderCommandeText"
+).textContent = `Merci de ta récente commande n° "${Id}" chez Orinoco.`;
+
+function deleteKey(key) {
+  localStorage.removeItem(key);
+}
+
+deleteKey("cart");
+deleteKey("idresponse");
+
+///Retour sur la page d'accueil après actualisation de la page confirmation
+// if (cart == null || idresponse == null) {
+//   window.location = "index.html";
+// }
